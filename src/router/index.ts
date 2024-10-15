1beee4c9682429a175da9f9f665480df0c6129c9
kernelcurry <kernelcurry@kernelcurrysAir.localdomain>
2024-10-15 00:23:12 -0700

    Add Dev view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BasicLayout from "@/layouts/BasicLayout.vue";
+import DevView from "@/views/DevView.vue";
+import BlankLayout from "@/layouts/BlankLayout.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,7 +21,13 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/dev',
+      name: 'dev',
+      meta: {layout: BlankLayout},
+      component: DevView
+    },
   ]
 })
 
---
+++ src/views/DevView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="m20">
+    <div class="absolute top-5 left-20">THIS IS A TEST</div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
