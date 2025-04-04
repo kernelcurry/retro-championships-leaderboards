812868113bd7580c5ff14922341a7d08dd4cb3ae
kernelcurry <kernelcurry@kernelcurrys-MacBook-Air.local>
2025-04-03 22:48:46 -0700

    Update Quals to have placment numbers

--- src/views/QualifiersView.vue
+++ src/views/QualifiersView.vue
@@ -70,7 +70,7 @@ const Top17Plus = computed(() => leaderboardsStore.allQualifiers.slice(16));
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
           <div class="col-span-1 md:col-span-2 xl:col-span-4 w-full text-center bg-slate-900 border-2 border-slate-400 rounded-t-xl py-2">
             <span class="inline-block w-full text-3xl font-bold">Top 8 Qualify for Finals!</span>
-            <span class="inline-block text-xl font-normal">(Join us Sunday @ 12:00)</span>
+            <span class="inline-block text-xl font-normal">(Join us Sunday @ 1:00pm)</span>
           </div>
           <LeaderboardRow
             v-for="(item, index) in Top1"
@@ -88,7 +88,7 @@ const Top17Plus = computed(() => leaderboardsStore.allQualifiers.slice(16));
             :name="item.name"
             :place="item.place"
             :scores="item.scores"
-            :use-place-styles="false"
+            :use-place-styles="true"
           />
         </div>
       </div>
@@ -97,7 +97,7 @@ const Top17Plus = computed(() => leaderboardsStore.allQualifiers.slice(16));
         <div class="grid grid-flow-row grid-rows-none md:grid-cols-2 lg:grid-cols-3 xl:grid-flow-col xl:grid-cols-3 xl:grid-rows-9 2xl:grid-rows-9">
           <div class="col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4 w-full text-center bg-slate-900 border-2 border-slate-400 rounded-t-xl py-2">
             <span class="inline-block w-full text-3xl font-bold">Top 9 - 32</span>
-            <span class="inline-block text-xl font-normal">Some inspirational quote here... YAY we Did it!</span>
+            <span class="inline-block text-xl font-normal">Play as many Qualifiers runs as you would like until Sunday @ 12:00</span>
           </div>
           <LeaderboardRow
             v-for="(item, index) in Top9to16"
